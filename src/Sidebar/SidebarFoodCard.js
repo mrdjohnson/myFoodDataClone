647ea86d9d70df6e60e0adcf91d6cedf0be6a5d5
DJ <djsm00thee3@gmail.com>
2020-04-25 14:02:21 -0700

    adding selectable information to the sidebar food card

--- src/Sidebar/SidebarFoodCard.js
+++ src/Sidebar/SidebarFoodCard.js
@@ -1,14 +1,29 @@
-import React, { useContext } from "react";
-
-import { Card, Tooltip, Button } from "antd";
-import { CloseOutlined } from "@ant-design/icons";
-
-import "./SidebarFoodCard.scss";
+import React, { useState, useEffect, useContext } from "react";
 import AppContext from "../AppContext";
 
+import { Card, Tooltip, Button, InputNumber, Row, Col, Select } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+
+import _ from "lodash";
+
+import "./SidebarFoodCard.scss";
+
+const { Option } = Select;
+
 function SidebarFoodCard({ foodItemData }) {
-  console.log("foodItemData:", foodItemData);
   const { clearFoodItemData } = useContext(AppContext);
+
+  const servingDescriptions = foodItemData["desc_arr"];
+  const servingWeights = foodItemData["wt_arr"].map(_.toInteger);
+  const servings = _.zip(servingDescriptions, servingWeights);
+
+  const [selectedServing, setSelectedServing] = useState(
+    _.first(servingDescriptions)
+  );
+
+  useEffect(() => {
+    setSelectedServing(_.first(servingDescriptions));
+  }, [servingDescriptions]);
 
   const closeButton = (
     <Tooltip title="close">
@@ -16,10 +31,22 @@ function SidebarFoodCard({ foodItemData }) {
         type="link"
         icon={<CloseOutlined />}
         onClick={clearFoodItemData}
-        className="sidebar-food-card-close-button"
+        className="sidebar-food-card__close-button"
       />
     </Tooltip>
   );
+
+  const renderServing = (serving) => {
+    const [description, weight] = serving;
+
+    return (
+      <Option value={weight} key={description}>
+        {description} {`(${weight}g)`}
+      </Option>
+    );
+  };
+
+  const handleChange = () => {};
 
   return (
     <Card
@@ -28,7 +55,31 @@ function SidebarFoodCard({ foodItemData }) {
       extra={closeButton}
       className="sidebar-food-card"
     >
-      innards go here
+      <Row className="sidebar-food-card__body" align="middle">
+        <Col span={2}>
+          <InputNumber
+            min={1}
+            defaultValue={3}
+            onChange={handleChange}
+            className="sidebar-food-card__serving-input"
+          />
+        </Col>
+
+        <Col span={2}>
+          <CloseOutlined />
+        </Col>
+
+        <Col span={20}>
+          <Select
+            value={selectedServing}
+            style={{ width: "100%" }}
+            dropdownMatchSelectWidth={false}
+            onChange={setSelectedServing}
+          >
+            {servings.map(renderServing)}
+          </Select>
+        </Col>
+      </Row>
     </Card>
   );
 }
