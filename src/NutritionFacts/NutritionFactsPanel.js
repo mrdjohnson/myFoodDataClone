85acd805b895f27fffd6d3dce87c8976685db937
DJ <djsm00thee3@gmail.com>
2020-04-27 15:13:53 -0700

    adding food data information to nutrition panel

--- src/NutritionFacts/NutritionFactsPanel.js
+++ src/NutritionFacts/NutritionFactsPanel.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import NutritionFactsTable from "./NutritionFactsTable";
 import { Row } from "antd";
 import NutritionFactsHistogram from "./NutritionFactsHistogram";
+import AppContext from "../AppContext";
+
+import "./NutritionFactsPanel.scss";
 
 export default function NutritionFactsPanel() {
+  const { foodItemData } = useContext(AppContext);
+
+  if (!foodItemData) return null;
   return (
-    <Row>
-      <NutritionFactsTable />
-      <NutritionFactsHistogram />
-    </Row>
+    <div className="facts-panel">
+      <h1 className="facts-panel-header">{foodItemData.name}</h1>
+      <div className="facts-panel-subheader">{foodItemData.longname}</div>
+      <div>
+        <strong>Database:</strong>
+        {foodItemData.datatype}
+      </div>
+      <Row align="middle" className="facts-panel-body">
+        <NutritionFactsTable />
+        <NutritionFactsHistogram />
+      </Row>
+    </div>
   );
 }
