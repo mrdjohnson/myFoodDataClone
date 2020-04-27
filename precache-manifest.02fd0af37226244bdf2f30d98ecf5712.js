3d52feeca99a1b32c82e0500ea0be63da2cf2f8c
DJ <djsm00thee3@gmail.com>
2020-04-27 16:10:56 -0700

    Updates

---
+++ precache-manifest.02fd0af37226244bdf2f30d98ecf5712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abe24ad73f033127f41e876f083b906",
+    "url": "/myFoodDataClone/index.html"
+  },
+  {
+    "revision": "9cdb30ef1326159acd83",
+    "url": "/myFoodDataClone/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "e0c6182c1163e86bbff3",
+    "url": "/myFoodDataClone/static/css/main.d96df32a.chunk.css"
+  },
+  {
+    "revision": "9cdb30ef1326159acd83",
+    "url": "/myFoodDataClone/static/js/2.6642d33d.chunk.js"
+  },
+  {
+    "revision": "e0c6182c1163e86bbff3",
+    "url": "/myFoodDataClone/static/js/main.3510b78c.chunk.js"
+  },
+  {
+    "revision": "3e98a868fbcd7dd3579b",
+    "url": "/myFoodDataClone/static/js/runtime-main.e6d317ac.js"
+  },
+  {
+    "revision": "8357fd405989f4cf28a496e9ff32afaa",
+    "url": "/myFoodDataClone/static/media/logo.8357fd40.svg"
+  }
+]);
