a0eb895d756cc5ac4b810fcae0e431819b6c957c
DJ <djsm00thee3@gmail.com>
2020-04-30 19:14:50 -0700

    Extracting the selection row so from the food card
    this will be used by the facts panel

---
+++ src/Sidebar/ServingSizeSelectionRow.js
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import AppContext from "../AppContext";
+
+import { InputNumber, Row, Col, Select } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+
+import _ from "lodash";
+
+import "./ServingSizeSelectionRow.scss";
+
+const { Option } = Select;
+
+const MIN_QUANTITY = 1;
+const MAX_QUANTITY = 10000;
+
+function ServingSizeSelectionRow({ foodItemData, className }) {
+  const {
+    updateFoodWithSelectedQuantity,
+    updateFoodWithSelectedWeight,
+  } = useContext(AppContext);
+
+  const { selectedQuantity, selectedWeightIndex } = foodItemData;
+  const servingDescriptions = foodItemData["desc_arr"];
+  const servingWeights = foodItemData["wt_arr"].map(_.toInteger);
+  const servings = _.zip(servingDescriptions, servingWeights).map(
+    ([description, weight]) => `${description} (${weight}g)`
+  );
+
+  const selectedWeight = servings[selectedWeightIndex];
+
+  const renderServing = (serving, index) => (
+    <Option value={index} key={serving}>
+      {serving}
+    </Option>
+  );
+
+  const updateSelectedQuantity = (selectedQuantity) => {
+    if (MIN_QUANTITY <= selectedQuantity && selectedQuantity < MAX_QUANTITY) {
+      updateFoodWithSelectedQuantity(foodItemData, selectedQuantity);
+    }
+  };
+
+  const updateSelectedWeightIndex = (selectedWeightIndex) => {
+    updateFoodWithSelectedWeight(foodItemData, selectedWeightIndex);
+  };
+
+  return (
+    <div className={className}>
+      <Row className="serving-size-selection-row__body" align="middle">
+        <Col span={4}>
+          <InputNumber
+            min={MIN_QUANTITY}
+            max={MAX_QUANTITY}
+            value={selectedQuantity}
+            placeholder={1}
+            onChange={updateSelectedQuantity}
+            className="serving-size-selection-row__serving-input"
+          />
+        </Col>
+
+        <Col span={2}>
+          <CloseOutlined />
+        </Col>
+
+        <Col flex={1}>
+          <Select
+            value={selectedWeight}
+            className="serving-size-selection-row__select"
+            style={{ width: "100%" }}
+            dropdownMatchSelectWidth={false}
+            onChange={updateSelectedWeightIndex}
+          >
+            {servings.map(renderServing)}
+          </Select>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ServingSizeSelectionRow;
--- src/Sidebar/SidebarFoodCard.js
+++ src/Sidebar/SidebarFoodCard.js
@@ -1,33 +1,17 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import AppContext from "../AppContext";
 
-import { Card, Tooltip, Button, InputNumber, Row, Col, Select } from "antd";
+import { Card, Tooltip, Button } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
+
+import ServingSizeSelectionRow from "./ServingSizeSelectionRow";
 
 import _ from "lodash";
 
 import "./SidebarFoodCard.scss";
 
-const { Option } = Select;
-
-const MIN_QUANTITY = 1;
-const MAX_QUANTITY = 10000;
-
 function SidebarFoodCard({ foodItemData }) {
-  const {
-    clearFoodItemData,
-    updateFoodWithSelectedQuantity,
-    updateFoodWithSelectedWeight,
-  } = useContext(AppContext);
-
-  const { selectedQuantity, selectedWeightIndex } = foodItemData;
-  const servingDescriptions = foodItemData["desc_arr"];
-  const servingWeights = foodItemData["wt_arr"].map(_.toInteger);
-  const servings = _.zip(servingDescriptions, servingWeights).map(
-    ([description, weight]) => `${description} (${weight}g)`
-  );
-
-  const selectedWeight = servings[selectedWeightIndex];
+  const { clearFoodItemData } = useContext(AppContext);
 
   const closeButton = (
     <Tooltip title="close">
@@ -40,22 +24,6 @@ function SidebarFoodCard({ foodItemData }) {
     </Tooltip>
   );
 
-  const renderServing = (serving, index) => (
-    <Option value={index} key={serving}>
-      {serving}
-    </Option>
-  );
-
-  const updateSelectedQuantity = (selectedQuantity) => {
-    if (MIN_QUANTITY <= selectedQuantity && selectedQuantity < MAX_QUANTITY) {
-      updateFoodWithSelectedQuantity(foodItemData, selectedQuantity);
-    }
-  };
-
-  const updateSelectedWeightIndex = (selectedWeightIndex) => {
-    updateFoodWithSelectedWeight(foodItemData, selectedWeightIndex);
-  };
-
   return (
     <Card
       title={foodItemData.name}
@@ -63,34 +31,7 @@ function SidebarFoodCard({ foodItemData }) {
       extra={closeButton}
       className="sidebar-food-card"
     >
-      <Row className="sidebar-food-card__body" align="middle">
-        <Col span={4}>
-          <InputNumber
-            min={MIN_QUANTITY}
-            max={MAX_QUANTITY}
-            value={selectedQuantity}
-            placeholder={1}
-            onChange={updateSelectedQuantity}
-            className="sidebar-food-card__serving-input"
-          />
-        </Col>
-
-        <Col span={2}>
-          <CloseOutlined />
-        </Col>
-
-        <Col span={18}>
-          <Select
-            value={selectedWeight}
-            className="sidebar-food-card__select"
-            style={{ width: "100%" }}
-            dropdownMatchSelectWidth={false}
-            onChange={updateSelectedWeightIndex}
-          >
-            {servings.map(renderServing)}
-          </Select>
-        </Col>
-      </Row>
+      <ServingSizeSelectionRow foodItemData={foodItemData} />
     </Card>
   );
 }
