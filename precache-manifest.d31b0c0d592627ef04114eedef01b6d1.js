5c39a8abc8d42276f074b789a19095eaebbbd320
djohnson <mr.demarcus.johnson@gmail.com>
2021-04-03 19:48:58 -0700

    Updates

--- precache-manifest.d31b0c0d592627ef04114eedef01b6d1.js
+++ precache-manifest.d31b0c0d592627ef04114eedef01b6d1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a98f27eab3724ecb09fba907770bc099",
+    "revision": "e0c3c01ecf0f0f81ba377110d0096fc8",
     "url": "/myFoodDataClone/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myFoodDataClone/static/css/2.e10332d9.chunk.css"
   },
   {
-    "revision": "af4ece347a3624796e7f",
+    "revision": "a70b34b6b5f6fa08c7eb",
     "url": "/myFoodDataClone/static/css/main.42bc29ec.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myFoodDataClone/static/js/2.ea4b6030.chunk.js"
   },
   {
-    "revision": "af4ece347a3624796e7f",
-    "url": "/myFoodDataClone/static/js/main.ca6b39fb.chunk.js"
+    "revision": "a70b34b6b5f6fa08c7eb",
+    "url": "/myFoodDataClone/static/js/main.a9c88905.chunk.js"
   },
   {
     "revision": "43143ed55ff33ddae3b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myFoodDataClone/precache-manifest.c748e81f6c3b5700ad77c6a36196aa8e.js"
+  "/myFoodDataClone/precache-manifest.d31b0c0d592627ef04114eedef01b6d1.js"
 );
 
 self.addEventListener('message', (event) => {
